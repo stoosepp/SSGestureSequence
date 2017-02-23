f4a3d27b46e8cef9d4416176df64fa1143287ce1
Stoo <stoosepp@stoos-air.lan.uow.edu.au>
2017-02-23 13:05:05 +1100

    Added Velocity Detection in line Color. Red is fast and yellow is normal speed.

--- TouchSequenceTest/ViewController.swift
+++ TouchSequenceTest/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     var lastPoint = CGPoint.zero
     var swiped = false
     var currentTouch = 0
+    
+    //Last Touch
+    var lastTouch = UITouch()
+    var maxVelocity:Double = 0.0
+    var minVelocity:Double = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +39,8 @@ class ViewController: UIViewController {
         swiped = false
         if touches.count != 0{
             let touch = touches.first! as UITouch
-            lastPoint = touch.location(in: self.view)
+            lastTouch = touch
+            lastPoint = touch.location(in: view)
             let green = UIColor.init(red: 167/255, green: 197/255, blue: 116/255, alpha: 1.0)
             addSequenceCount(atLocation: lastPoint, radius: 10, color:green)
         }
@@ -46,8 +52,17 @@ class ViewController: UIViewController {
         if touches.count != 0{
             let touch = touches.first! as UITouch
             let currentPoint = touch.location(in: view)
-            drawLineFrom(lastPoint, toPoint: currentPoint)
+            //Try Velocity
+            let distanceBetween = distance(a: lastPoint, b: currentPoint)
+            let timeBetween = touch.timestamp - lastTouch.timestamp
+            //print(touch.timestamp)
+            //print(lastTouch.timestamp)
+            let velocity = Double(distanceBetween)/timeBetween
+            //print("Distance:\(distanceBetween) Time:\(timeBetween) Velocity: \(velocity)")
+            drawLineFrom(lastPoint, toPoint: currentPoint, withDistance: Double(distanceBetween))
             lastPoint = currentPoint
+            lastTouch = touch
+            
             
         }
         
@@ -56,7 +71,7 @@ class ViewController: UIViewController {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if !swiped {
             // draw a single point
-            drawLineFrom(lastPoint, toPoint: lastPoint)
+            drawLineFrom(lastPoint, toPoint: lastPoint, withDistance: 0.0)
         }
         let red = UIColor.init(red: 190/255, green: 75/255, blue: 85/255, alpha: 1.0)
         addSequenceCount(atLocation: lastPoint, radius: 10, color:red)
@@ -69,6 +84,12 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext()
         tempImageView.image = nil
      
+    }
+    
+    func distance(a: CGPoint, b: CGPoint) -> CGFloat {
+        let xDist = a.x - b.x
+        let yDist = a.y - b.y
+        return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
     }
     
     func addSequenceCount(atLocation:CGPoint, radius:CGFloat, color:UIColor){
@@ -84,7 +105,7 @@ class ViewController: UIViewController {
     }
     
     
-    func drawLineFrom(_ fromPoint: CGPoint, toPoint: CGPoint) {
+    func drawLineFrom(_ fromPoint: CGPoint, toPoint: CGPoint, withDistance:Double) {
         
         // 1
         UIGraphicsBeginImageContext(view.frame.size)
@@ -98,7 +119,22 @@ class ViewController: UIViewController {
         // 3
         context!.setLineCap(CGLineCap.round)
         context!.setLineWidth(5.0)
-        context!.setStrokeColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+        //context!.setStrokeColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)//Gray
+        //context!.setStrokeColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)//Black
+
+        //Record Velocity
+        //Flat Yellow 247,203,87
+        //Flat Red 229,77,66
+
+        let redDiff = (withDistance/50) * (247-229)
+        let greenDiff = (withDistance/50) * (203-77)
+        let blueDiff = (withDistance/50) * (87-66)
+        let redValue = CGFloat(247 - redDiff)/255
+        let greenValue = CGFloat(203 - greenDiff)/255
+        let blueValue = CGFloat(87 - blueDiff)/255
+        
+        context!.setStrokeColor(red: redValue, green:greenValue , blue: blueValue, alpha: 1.0)
+
         context!.setBlendMode(CGBlendMode.normal)
         
         // 4
@@ -106,7 +142,7 @@ class ViewController: UIViewController {
         
         // 5
         tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        tempImageView.alpha = 0.0
+        //tempImageView.alpha = 0.0//Unhide this for production
         
         UIGraphicsEndImageContext()
     }
