50b15ae52d2fc90810c226672063ff5e8b4c2b3e
Stoo <stoosepp@stoos-air.lan.uow.edu.au>
2017-03-17 11:40:09 +1100

    Added quantification of gesture speed based on color, including slow medium and fast gestures, percentage of each and average line segment length.

--- TouchSequenceTest/ViewController.swift
+++ TouchSequenceTest/ViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var velocityLabel: UILabel!
     @IBOutlet weak var tempImageView: UIImageView!
     @IBOutlet weak var mainImageView: UIImageView!
     
     var lastPoint = CGPoint.zero
     var swiped = false
     var currentTouch = 0
-    
-    //Last Touch
-    var lastTouch = UITouch()
-    var maxVelocity:Double = 0.0
-    var minVelocity:Double = 0.0
+
+    var slowCount = 0.0
+    var mediumCount = 0.0
+    var fastCount = 0.0
+
+    var slowDistance = 0.0
+    var mediumDistance = 0.0
+    var fastDistance = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        velocityLabel.text = "Slow: 0%\nMedium: 0%\nFast: 0%\nAvg: 0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,7 +44,7 @@ class ViewController: UIViewController {
         swiped = false
         if touches.count != 0{
             let touch = touches.first! as UITouch
-            lastTouch = touch
+            //lastTouch = touch
             lastPoint = touch.location(in: view)
             let green = UIColor.init(red: 167/255, green: 197/255, blue: 116/255, alpha: 1.0)
             addSequenceCount(atLocation: lastPoint, radius: 10, color:green)
@@ -52,16 +57,10 @@ class ViewController: UIViewController {
         if touches.count != 0{
             let touch = touches.first! as UITouch
             let currentPoint = touch.location(in: view)
-            //Try Velocity
+
             let distanceBetween = distance(a: lastPoint, b: currentPoint)
-            let timeBetween = touch.timestamp - lastTouch.timestamp
-            //print(touch.timestamp)
-            //print(lastTouch.timestamp)
-            let velocity = Double(distanceBetween)/timeBetween
-            //print("Distance:\(distanceBetween) Time:\(timeBetween) Velocity: \(velocity)")
             drawLineFrom(lastPoint, toPoint: currentPoint, withDistance: Double(distanceBetween))
             lastPoint = currentPoint
-            lastTouch = touch
             
             
         }
@@ -125,7 +124,45 @@ class ViewController: UIViewController {
         //Record Velocity
         //Flat Yellow 247,203,87
         //Flat Red 229,77,66
-
+        if withDistance < 10{
+            slowDistance += withDistance
+            slowCount += 1
+        }
+        else if withDistance > 10 && withDistance < 30{
+            mediumDistance += withDistance
+            mediumCount += 1
+        }
+        else if withDistance > 30{
+            fastDistance += withDistance
+            fastCount += 1
+        }
+        
+        let totalSegment = slowCount + mediumCount + fastCount
+        let totalDistance = slowDistance + mediumDistance + fastDistance
+        
+        let totalCombined = (slowCount * slowDistance) +  (mediumCount * mediumDistance) + (fastCount * fastDistance)
+        var slowPercentage = (slowCount * slowDistance) / totalCombined
+        var mediumPercentage =  (mediumCount * mediumDistance) / totalCombined
+        var fastPercentage = (fastCount * fastDistance) / totalCombined
+        let avgDistance = totalDistance / totalSegment
+        if slowPercentage.isNaN{
+            slowPercentage = 0.0
+        }
+        if mediumPercentage.isNaN{
+            mediumPercentage = 0.0
+        }
+        if fastPercentage.isNaN{
+            fastPercentage = 0.0
+        }
+        
+        let slowString = String(format: "Slow: %.2f", slowPercentage * 100)
+        let mediumString = String(format: "Medium: %.2f", mediumPercentage * 100)
+        let fastString = String(format: "Fast: %.2f", fastPercentage * 100)
+        let avgString = String(format: "Avg: %.2f", avgDistance)
+        print("TotalSegments:\(totalSegment)\nSlowSegments:\(slowCount)")
+        
+        velocityLabel.text = "\(slowString)%\n\(mediumString)%\n\(fastString)%\n\(avgString)pts"
+        
         let redDiff = (withDistance/50) * (247-229)
         let greenDiff = (withDistance/50) * (203-77)
         let blueDiff = (withDistance/50) * (87-66)
@@ -145,6 +182,25 @@ class ViewController: UIViewController {
         //tempImageView.alpha = 0.0//Unhide this for production
         
         UIGraphicsEndImageContext()
+    }
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print("Device was shaken!")
+       
+        for view in tempImageView.subviews{
+            view.removeFromSuperview()
+        }
+        tempImageView.image = nil
+        mainImageView.image = nil
+        currentTouch = 0
+        velocityLabel.text = "Slow: 0%\nMedium: 0%\nFast: 0%\nAvg: 0pts"
+       slowCount = 0.0
+        mediumCount = 0.0
+        fastCount = 0.0
+        
+        slowDistance = 0.0
+        mediumDistance = 0.0
+        fastDistance = 0.0
     }
 
 
